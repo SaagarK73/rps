3d983d37c6107f955b9c11411952714ad7b49445
unknown <saagarkalaskar@gmail.com>
2023-05-30 18:48:00 +0530

    adding buttons

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function getUserChoice() {
-    const input = prompt("Choose between Rock, Papper & Scissor", "rock");
-    return input.toLowerCase();
+function play(e) {
+    
+    const compChoice = getComputerChoice();
+    const userChoice = e.target.innerText.toLowerCase();
+
+    const winnerElement = document.querySelector('.winner');
+
+    let shouldContinue = isGameRunning(playerScore, compScore);
+
+    if(shouldContinue) {
+        let winner = getWinner(userChoice, compChoice);
+        if (winner === 'tie') {
+            winnerElement.textContent = `its a tie !!`;
+        }else {
+            winnerElement.textContent = `${winner} wins this round !!`;
+        }
+        updateWinnerScore(winner);
+    }
 }
 
 function getComputerChoice(){
@@ -10,7 +25,8 @@ function getComputerChoice(){
 }
 
 function getWinner(userChoice, compChoice) {
-    console.log(`user choice is ${userChoice} and comp choice is ${compChoice}`);
+    updateChoicesOnUI(userChoice, compChoice);
+
     if(userChoice === compChoice) {
         return "tie";
     } else if(userChoice === 'rock'){
@@ -34,26 +50,20 @@ function getWinner(userChoice, compChoice) {
     }
 }
 
-function playGame() {
-    let playerScore = 0, compScore = 0;
-    let shouldContinue = isGameRunning(playerScore, compScore);
-
-    while (shouldContinue) {
-        let winner = restartGame();
-        console.log(winner);
+function updateWinnerScore(winner) {
+        
         if(winner === "user"){
-            playerScore++;
+            playerScore += 1;
+            updateRoundWinnerUI(winner, playerScore);
         }else if(winner === "comp"){
-            compScore++;
+            compScore += 1;
+            updateRoundWinnerUI(winner, compScore);
         }
-        shouldContinue = isGameRunning(playerScore, compScore);
-        console.log(`${shouldContinue} ======= ${playerScore} ======${compScore}`);
-    }
 
     if(playerScore === 5){
-        console.log("Player wins the game");
+        updateUltimateWinner("Player");
     }else if (compScore === 5){
-        console.log("Comp wins the game");
+        updateUltimateWinner("Computer");
     }
 }
 
@@ -61,10 +71,22 @@ function isGameRunning(playerScore, compScore) {
     return (playerScore < 5 && compScore < 5);
 }
 
-function restartGame() {
-    const userChoice = getUserChoice();
-    const compChoice = getComputerChoice();
-    return getWinner(userChoice, compChoice);
+function updateChoicesOnUI(userChoice, compChoice) {
+    const element = document.querySelector('.choices');
+    element.textContent = `Your Choice is ${userChoice} and Computer selected ${compChoice}`;
 }
 
-playGame();
+function updateRoundWinnerUI(winner, score){
+    const element = document.querySelector(`.${winner}.wins`);
+    element.textContent = `${winner}: ${score}`;
+}
+
+function updateUltimateWinner(winner) {
+    const element = document.querySelector('.ultimateWinner');
+    element.textContent = `${winner} is the winner`;
+}
+
+// playGame();
+let playerScore = 0, compScore = 0;
+let elements = document.querySelectorAll('.button');
+elements.forEach(item => item.addEventListener('click', play));
